3dda64d8060bc8f7bb8fda8f9442203d95a6d608
Nathin2005 <palanijayanthi2@gmail.com>
2026-02-17 10:04:59 +0530

    Create demo.py

---
+++ demo.py
@@ -0,0 +1,128 @@
+# apple_fruit = 10 
+# print(apple_fruit)
+# _apple = 10
+# print(_apple)
+# appleFruit = 10 
+# print (appleFruit)
+# AppleFruuit = 10
+# print(AppleFruuit)
+# APPLE = 10
+# print(APPLE)
+
+# DATA TYPE 
+
+# 1.int 
+# 2.float
+# 3.str
+# 4.bool
+
+# operator
+
+# birth_year = 2005
+# future_year = 2030
+# age = future_year - birth_year
+# print(age)
+
+# current_age = 21
+# maximum_age = 31
+# estimated_amount_per_day = 1000
+# total_eaten = (maximum_age - current_age)*365*estimated_amount_per_day
+# print (total_eaten)
+
+# LIFETIME WATER DRINKING CALCULATOR
+
+# current_age = 21
+# max_age = 80
+# litre_water = 5 
+# year_left = max_age - current_age
+# days_left = year_left*365
+# total_water = days_left * litre_water
+# print (total_water)
+
+# TOTAL FUEL COST FOR THE TRIP 
+
+# distance = 300
+# mileage = 15
+# petrol_price = 100
+# fuel_needed = distance / mileage
+# total_cost = fuel_needed * petrol_price
+# print (fuel_needed )
+# print (total_cost )
+
+# data = 5 
+# days = 30 
+# total_month = data*days 
+# print (total_month)
+
+# AGE IN DAYS
+
+# current_age = 21
+# years  = current_age * 365 
+# print (years)
+
+
+# original_price = 1000
+# discount_percentage = 20
+# decimal = discount_percentage / 100
+# discount_amount = original_price * decimal
+# final_price = original_price - discount_amount
+# print(decimal)
+# print(discount_amount)
+# print(final_price)
+
+# AREA OF WALL
+
+# length = 10
+# breath = 20 
+# area_of_wall = length*breath
+# print ("area_of_wall ")
+
+
+# x = 10
+# y = 5
+# if x == y:
+#     print(x," is equal to ",y)
+# elif x > y:
+#     print(x," is greater than" ,y)
+# else:
+#     print(x ,"is less than ",y)
+
+
+# ch = input("Enter a character: ")
+# if ch in ['a', 'e', 'i', 'o', 'u']:
+#     print(" vowel")
+# else:
+#     print("It is a consonant")
+
+# Automated Grading System
+
+# mark = int(input("Enter student mark: "))
+# if mark == 100:
+#     grade = "S"
+# elif 90 <= mark <= 99:
+#     grade = "A"
+# elif 80 <= mark <= 89:
+#     grade = "B"
+# elif 70 <= mark <= 79:
+#     grade = "C"
+# elif 60 <= mark <= 69:
+#     grade = "D"
+# elif 50 <= mark <= 59:
+#     grade = "E"
+# else :
+#     grade = "F"
+# print("Grade:", grade)
+
+
+# PROFIT OR LOSS FOR FRUIT SELLER 
+    
+x = float(input("Enter the cost price of 1 dozen bananas: "))
+y = float(input("Enter the selling price of 1 banana: "))
+total_selling_price = 12 * y
+profit_or_loss = total_selling_price - x
+if profit_or_loss > 0:
+    print("Profit =", profit_or_loss)
+elif profit_or_loss < 0:
+    print("Loss =", (profit_or_loss))
+else:
+    print("No Profit No Loss")
